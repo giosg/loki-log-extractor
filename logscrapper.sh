332cfce0ea558d3f3711120db126b599db86328a
Denis Ananev <denis.ananev@giosg.com>
2023-05-30 13:18:09 +0300

    fix: Improve script's configuration

--- logscrapper.sh
+++ logscrapper.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e 
-: ${LOGCLI_PATH:="/usr/local/bin/logcli"} 
-: ${LOKI_ADDR:="http://k8s-loki-gateway-loki.service.fsn1-sb.giosg.local:30101"} 
-: ${START_TIME:="$(date -d 'yesterday' -u +%Y-%m-%dT%H:%M:%SZ)"}
-: ${END_TIME:="$(date -u +%Y-%m-%dT%H:%M:%SZ)"}
-: ${GPG_RECEPIENT:="s3logs@giosg.com"}
-: ${S3_BUCKET:="loki-minio-logs"}
-: ${OUTPUT_FILE:="$(date --date='yesterday' '+%F').txt"}
-: ${COMPRESSED_FILE:="$OUTPUT_FILE.xz"}
-: ${ENCRYPTED_FILE:="$COMPRESSED_FILE.gpg"}
+LOGCLI_PATH="/usr/local/bin/logcli" 
+: ${LOKI_ADDR:="http://loki"} 
+: ${INPUT_TIME:="yesterday 23:00"}
+: ${OUTPUT_TIME:="today 09:00"}   
+: ${GPG_RECIPIENT:="gpg@giosg.com"}
+: ${S3_PATH:="bucket"}
+OUTPUT_FILE="$(date --date='yesterday' '+%F').txt"
+COMPRESSED_FILE="$OUTPUT_FILE.xz"
+ENCRYPTED_FILE="$COMPRESSED_FILE.gpg"
 : ${MAX_ARCHIVE_SIZE:=10485760}
 : ${GPG_PUBLIC_KEY_PATH:=secrets/gpg_public_key.asc}
 : ${PART_PATH_PREFIX:=/tmp/my_query}
@@ -18,10 +18,22 @@ set -e
 #Preliminary checks
 
 QUERY="$1" 
+START_TIME=$2
+END_TIME=$3
+
+INPUT_TIME="$(date -d "$START_TIME" -u +%Y-%m-%dT%H:%M:%SZ)"
+OUTPUT_TIME="$(date -d "$END_TIME" -u +%Y-%m-%dT%H:%M:%SZ)"
+
 
 # Check if the logcli query is empty
 if [ -z "$QUERY" ]; then
     echo "Logcli query is empty."
+    exit 1
+fi
+
+# Check if the time slots are empty
+if [[ -z "$START_TIME"|| -z "$END_TIME" ]]; then
+    echo "The time slots are wrong"
     exit 1
 fi
 
@@ -35,7 +47,7 @@ gpg --import "$GPG_PUBLIC_KEY_PATH"
 if [ -d "$(dirname "$PART_PATH_PREFIX")" ]; then
    FILENAME_PREFIX=$(echo "$QUERY" | tr -d '\"\/_')
     # Run logcli with the specified query and time range, and save the output to a file
-    "$LOGCLI_PATH"   query "$QUERY" --timezone=UTC --from="$START_TIME" --to="$END_TIME"  --output=default --parallel-duration="5m" \
+    "$LOGCLI_PATH"   query "$QUERY" --timezone=UTC --from="$INPUT_TIME" --to="$OUTPUT_TIME"  --output=default --parallel-duration="5m" \
       --part-path-prefix="$PART_PATH_PREFIX" --merge-parts --parallel-max-workers="$PARALLEL_MAX_WORKERS" --quiet > "$OUTPUT_FILE"
 
     # Check if logcli executed successfully
@@ -50,14 +62,14 @@ if [ -d "$(dirname "$PART_PATH_PREFIX")" ]; then
 
     # Check if the archive file exists and has the correct XZ format
     if [ -f "$COMPRESSED_FILE" ]  && [ "$FILE_SIZE" -lt "$MAX_ARCHIVE_SIZE" ]; then # Remove xz --test "$COMPRESSED_FILE" 2>/dev/null
-    gpg --trust-model always --output "$ENCRYPTED_FILE" --encrypt --recipient "$GPG_RECEPIENT" "$COMPRESSED_FILE"      #Add recepient as configurable
+    gpg --trust-model always --output "$ENCRYPTED_FILE" --encrypt --recipient "$GPG_RECIPIENT" "$COMPRESSED_FILE"      #Add recepient as configurable
     else
         echo "Archive file doesn't exist or the size of archive is more than 10 MB"
         exit 1;
     fi
 
-    if aws s3 ls "s3://$S3_BUCKET" 2>/dev/null; then
-            aws s3 cp "$ENCRYPTED_FILE"  "s3://$S3_BUCKET/${FILENAME_PREFIX}_${ENCRYPTED_FILE}"
+    if aws s3 ls "s3://$S3_PATH" 2>/dev/null; then
+            aws s3 cp "$ENCRYPTED_FILE"  "s3://$S3_PATH/${FILENAME_PREFIX}_${ENCRYPTED_FILE}"
     else
         echo "AWS S3 bucket doesn't exist."
         exit 1;
